21edff987a9d5b92b19ccbeae29ebac7d14e70e2
Akunesia <54620575+HafizahM@users.noreply.github.com>
2020-03-22 00:37:37 +0700

    Add files via upload

---
+++ koneksi.php
@@ -0,0 +1,8 @@
+<?php 
+//set time zone location sesuai negara, jadikan Asia Jakarta date_default_timezone_set('Asia/Jakarta'); 
+
+//set koneksi ke server sesuai host, user, password dan database $server="localhost"; 
+$user="maulidya_tutorialweb"; 
+$pass="Decha211213*"; $database="maulidya_tutorialweb"; 
+
+//koneksikan ke server $conect=mysqli_connect($server,$user,$pass,$database) or die('Error Connection Network');
